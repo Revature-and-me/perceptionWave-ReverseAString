b921a650a3ecb86069c7840bba8e0fd28fef88f5
Jericho Nasser <jerichonasser@yahoo.com>
2023-10-22 16:07:44 +0000

    JN

--- src/main/java/StringReverse.java
+++ src/main/java/StringReverse.java
@@ -1,4 +1,4 @@
-
+import java.lang.StringBuilder;;
 public class StringReverse {
     /**
      * Task: return the reverse of a string.
@@ -13,6 +13,8 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
+        StringBuilder sb = new StringBuilder(str);
+        sb = sb.reverse();
+        return sb.toString();
     }
 }
